051048d336b345855dda328a7fa4ff794ef1657f
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2018-09-19 10:01:19 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,27 @@
+class Book
+  def initialize (title)
+    @title = title
+  end
 
+  def title
+    @title
+  end
 
+  def author=(author)
+  @author = author
+  end
+
+  def author
+    @author
+  end
+
+  attr_reader :page_count
+  attr_writer :page_count
+
+  attr_accessor :genre
+
+  def turn_page
+  puts "Flipping the page...wow, you read fast!"
+  end
+
+end
